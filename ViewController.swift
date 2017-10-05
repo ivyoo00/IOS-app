18135f1fac43f609876f06390329830bbbd07cfb
Jiali Ge <ivydany@bu.edu>
2017-10-05 15:03:55 -0400

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 葛伽丽 on 10/5/17.
+//  Copyright © 2017 葛伽丽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
