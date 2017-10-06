5045ab742e51708ed0a99068e40bc63135ef83f7
Jiali Ge <ivydany@bu.edu>
2017-10-06 16:20:54 -0400

    Create RatingControl.swift

---
+++ RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by 葛伽丽 on 10/6/17.
+//  Copyright © 2017 葛伽丽. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
